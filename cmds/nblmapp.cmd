d47a002235d711f86e17f787be3b95f14df8ab7c
Yannick <ymariett@localhost.localdomain>
2020-03-21 18:07:24 +0100

    Modification for managing several IFC1410

--- cmds/nblmapp.cmd
+++ cmds/nblmapp.cmd
@@ -11,27 +11,32 @@ require nblmapp,develop
 # Constant definitions
 epicsEnvSet(TRIG0_PV,           "$(TRIG0_PV=MTCA-EVR:EvtECnt-I.TIME)")
 epicsEnvSet(TIMESTAMP,          "$(TIMESTAMP=MTCA-EVR:Time-I.TIME)")
-epicsEnvSet(PREFIX,             "$(PREFIX=FEBx)")
-epicsEnvSet(DEVICE,             "$(DEVICE=PBI-nBLM)")
+epicsEnvSet(PREFIX,             "$(PREFIX=PBI-nBLM00)")
+epicsEnvSet(DEVICE,             "$(DEVICE=PBI-AMC-)")
+# The tens of AMC gives the card 0
+epicsEnvSet(AMC,                "$(AMC=100)")
+
 epicsEnvSet(EPICS_CA_MAX_ARRAY_BYTES, 400000000)
 
 # Set maximum number of samples: SCOPE_RAW_DATA_SAMPLES_MAX for the scope in the code
 epicsEnvSet(NELM, 5000)
 
 # ################## acquisition  ##############################
-ndsCreateDevice(ifc14, ${PREFIX}, card=0, fmc=1, chGrp=${DEVICE})
+ndsCreateDevice(nblm, $(PREFIX), chGrp=${DEVICE}, grpNb=${AMC})
 
 dbLoadRecords("trigTime.db", "TIMESTAMP=${TRIG0_PV}")
-dbLoadRecords("nblm_group.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE},TIMESTAMP=${TIMESTAMP}")
-dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE},CH_ID=CH0, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
-dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE},CH_ID=CH1, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
-dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE},CH_ID=CH2, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
-dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE},CH_ID=CH3, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
-dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE},CH_ID=CH4, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
-dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE},CH_ID=CH5, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
+
+
+dbLoadRecords("nblm_group.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE}${AMC},TIMESTAMP=${TIMESTAMP}")
+dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE}${AMC},CH_ID=CH0, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
+dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE}${AMC},CH_ID=CH1, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
+dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE}${AMC},CH_ID=CH2, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
+dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE}${AMC},CH_ID=CH3, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
+dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE}${AMC},CH_ID=CH4, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
+dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE}${AMC},CH_ID=CH5, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
 
 iocInit
 
 # neutron detection and scope configuration
 # Pulse processing
-dbpf ${PREFIX}:${DEVICE}-STAT "ON"
+dbpf ${PREFIX}:${DEVICE}${AMC}:STAT "ON"
--- cmds/nblmapp_concurrent.cmd
+++ cmds/nblmapp_concurrent.cmd
@@ -12,8 +12,19 @@ require nds3epics,1.0.1
 epicsEnvSet(TRIG0_PV,           "$(TRIG0_PV=MTCA-EVR:EvtECnt-I.TIME)")
 #epicsEnvSet(TIMESTAMP,          "null")
 epicsEnvSet(TIMESTAMP,          "$(TIMESTAMP=MTCA-EVR:Time-I.TIME)")
-epicsEnvSet(PREFIX,             "$(PREFIX=FEBx)")
-epicsEnvSet(DEVICE,             "$(DEVICE=PBI-nBLM)")
+epicsEnvSet(PREFIX,             "$(PREFIX=PBI-nBLM00)")
+epicsEnvSet(DEVICE,             "$(DEVICE=PBI-AMC-)")
+# The tens of AMC gives the card in slot 3
+epicsEnvSet(AMC1,               "$(AMC1=130)")
+# The tens of AMC gives the card in slot 5
+epicsEnvSet(AMC2,               "$(AMC2=150)")
+##################### Only one board ##############################
+#epicsEnvSet(AMCs,               "$(AMC1)")
+################# or with several IFC1410 #########################
+epicsEnvSet(AMCs,               "$(AMC1)_$(AMC2)")
+###################################################################
+
+
 epicsEnvSet(EPICS_CA_MAX_ARRAY_BYTES, 400000000)
 
 # Set maximum number of samples: SCOPE_RAW_DATA_SAMPLES_MAX for the scope in the code
@@ -22,22 +33,36 @@ epicsEnvSet(NELM, 5000)
 var onAMCOne 1
 
 # ################## acquisition  ##############################
-# IFC1410 in AMC Slot 5
-ndsCreateDevice(ifc14, ${PREFIX}, card=5, fmc=1, chGrp=${DEVICE})
-# For test with previous tsclib 3.1.0 (slot nb was always 0)
-#ndsCreateDevice(ifc14, ${PREFIX}, card=0, fmc=1, chGrp=${DEVICE})
+# IFC1410 in AMC Slot 3 and 5
+ndsCreateDevice(nblm, $(PREFIX), chGrp=${DEVICE}, grpNb=${AMCs})
 
 dbLoadRecords("trigTime.db", "TIMESTAMP=${TRIG0_PV}")
-dbLoadRecords("nblm_group.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE},TIMESTAMP=${TIMESTAMP}")
-dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE},CH_ID=CH0, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
-dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE},CH_ID=CH1, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
-dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE},CH_ID=CH2, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
-dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE},CH_ID=CH3, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
-dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE},CH_ID=CH4, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
-dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE},CH_ID=CH5, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
+
+
+dbLoadRecords("nblm_group.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE}${AMC1},TIMESTAMP=${TIMESTAMP}")
+dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE}${AMC1},CH_ID=CH0, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
+dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE}${AMC1},CH_ID=CH1, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
+dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE}${AMC1},CH_ID=CH2, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
+dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE}${AMC1},CH_ID=CH3, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
+dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE}${AMC1},CH_ID=CH4, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
+dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE}${AMC1},CH_ID=CH5, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
+
+################# Test with a 2nd IFC1410 #########################
+dbLoadRecords("nblm_group.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE}${AMC2},TIMESTAMP=${TIMESTAMP}")
+dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE}${AMC2},CH_ID=CH0, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
+dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE}${AMC2},CH_ID=CH1, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
+dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE}${AMC2},CH_ID=CH2, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
+dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE}${AMC2},CH_ID=CH3, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
+dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE}${AMC2},CH_ID=CH4, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
+dbLoadRecords("nblm.db", "PREFIX=${PREFIX},CH_GRP_ID=${DEVICE}${AMC2},CH_ID=CH5, NELM=${NELM},TIMESTAMP=${TIMESTAMP}")
+###################################################################
 
 iocInit
 
 # neutron detection and scope configuration
 # Pulse processing
-dbpf ${PREFIX}:${DEVICE}-STAT "ON"
+dbpf ${PREFIX}:${DEVICE}${AMC1}:STAT "ON"
+
+################# Test with a 2nd IFC1410 #########################
+dbpf ${PREFIX}:${DEVICE}${AMC2}:STAT "ON"
+###################################################################
